24a70570c25daba6be6b2679ea19a9a7b3d5183f
clairgustafson <clairgustafson@192.168.1.26>
2020-06-03 11:59:47 -0700

    First commit

---
+++ ClairGustafsonProject/src/App.java
@@ -0,0 +1,59 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double itemPrice1 = 88.95;
+		double itemPrice2 = 9.76;
+		System.out.println(itemPrice1);
+		System.out.println(itemPrice2);
+		
+		int walletAmount1 = 100;
+		int walletAmount2 = 25;
+		System.out.println(walletAmount1);
+		System.out.println(walletAmount2);
+				
+		//amount of money in wallet after item is purchased
+		System.out.println(walletAmount1 - itemPrice1);
+		
+		int numberOfFriends2019 = 5;
+		System.out.println(numberOfFriends2019);
+		
+		numberOfFriends2019 = numberOfFriends2019 + 2;
+		System.out.println(numberOfFriends2019);
+		
+		int numberOfFriends2020 = 7;
+		System.out.println(numberOfFriends2020);
+		
+		int age1 = 29;
+		int age2 = 30;
+		System.out.println(age1);
+		System.out.println(age2);
+		
+		//number of friends I've made each year based on my age variable and my number of friends variable
+		System.out.println();
+		
+		String firstName1 = "Clair";
+		String firstName2 = "Derek";
+		System.out.println(firstName1);
+		System.out.println(firstName2);
+		
+		String lastName1 = "Gustafson";
+		String lastName2 = "Jackson";
+		System.out.println(lastName1);
+		System.out.println(lastName2);
+		
+		char middleInitial1 = 'J';
+		char middleInitial2 = 'M';
+		System.out.println(middleInitial1);
+		System.out.println(middleInitial2);
+		
+		//Full name
+		System.out.println("My name is" + ' ' + firstName1 + ' ' + middleInitial1 + ' ' + lastName1);
+		
+		
+
+	}
+
+}

50a57886da3d103b5b9b6f6733d5b3b9e726e040
clairgustafson <clairgustafson@localhost>
2020-06-04 09:35:55 -0700

    Commit 2

--- ClairGustafsonProject/src/App.java
+++ ClairGustafsonProject/src/App.java
@@ -4,52 +4,55 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//price of items
 		double itemPrice1 = 88.95;
 		double itemPrice2 = 9.76;
-		System.out.println(itemPrice1);
-		System.out.println(itemPrice2);
+		System.out.println("The price of the black purse is" + " " + itemPrice1);
+		System.out.println("The price of the movie ticket is" + " " + itemPrice2);
 		
+		//amount of money in wallet
 		int walletAmount1 = 100;
 		int walletAmount2 = 25;
-		System.out.println(walletAmount1);
-		System.out.println(walletAmount2);
+		System.out.println("I have" + " " + "$" + walletAmount1 + " " + "in my wallet.");
+		System.out.println("My husabnd has" + " " + "$" + walletAmount2 + " " + "in his wallet.");
 				
-		//amount of money in wallet after item is purchased
+		//A. amount of money in wallet after item is purchased
 		System.out.println(walletAmount1 - itemPrice1);
 		
-		int numberOfFriends2019 = 5;
-		System.out.println(numberOfFriends2019);
+		//number of friends
+		int numberOfFriends2019 = 13;
+		int numberOfFriends2020 = 15;
+		System.out.println("In 2019 I had" + " " + numberOfFriends2019 + " " + "friends.");
+		System.out.println("In 2020 I had" + " " + numberOfFriends2020 + " " + "friends.");
 		
-		numberOfFriends2019 = numberOfFriends2019 + 2;
-		System.out.println(numberOfFriends2019);
-		
-		int numberOfFriends2020 = 7;
-		System.out.println(numberOfFriends2020);
-		
+		//age in years
 		int age1 = 29;
 		int age2 = 30;
-		System.out.println(age1);
-		System.out.println(age2);
+		System.out.println("I am" + " " + age1 + " " + "years old.");
+		System.out.println("I will be" + " " + age2 + " " + "years old in December.");
 		
-		//number of friends I've made each year based on my age variable and my number of friends variable
-		System.out.println();
+		//B. number of friends I've made each year based on my age variable and my number of friends variable
+		System.out.println(age1 / numberOfFriends2019);
 		
+		//first names
 		String firstName1 = "Clair";
 		String firstName2 = "Derek";
-		System.out.println(firstName1);
-		System.out.println(firstName2);
+		System.out.println("My first name is" + " " + firstName1);
+		System.out.println("My husband's first name is" + " " + firstName2);
 		
+		//last names
 		String lastName1 = "Gustafson";
 		String lastName2 = "Jackson";
-		System.out.println(lastName1);
-		System.out.println(lastName2);
+		System.out.println("My last name is" + " " + lastName1);
+		System.out.println("My maiden name is" + " " + lastName2);
 		
+		//middle initials
 		char middleInitial1 = 'J';
 		char middleInitial2 = 'M';
-		System.out.println(middleInitial1);
-		System.out.println(middleInitial2);
+		System.out.println("My middle initial is" + " " + middleInitial1);
+		System.out.println("My husband's middle initial is" + " " + middleInitial2);
 		
-		//Full name
+		//C. Full name
 		System.out.println("My name is" + ' ' + firstName1 + ' ' + middleInitial1 + ' ' + lastName1);
 		
 		
